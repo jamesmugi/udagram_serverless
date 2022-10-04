9f4d1280d7bf3b3e011386296f3fa3a67f3f728c
jamesmugi <jameskaranja109@gmail.com>
2022-10-04 16:00:52 +0300

    Almost final

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = 'vo0z3kaxxh';
+const apiId = '2nn4m0zgv9';
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
 
 export const authConfig = {
-  domain: 'dev-fjp-3wf9.us.auth0.com',
-  clientId: 'b4bG4TV8cDkqvafjImlMeEE4qUctL7Ti',
+  domain: 'dev-9absbsey.us.auth0.com',
+  clientId: 'YolnVKWSjOZROcYIYSlKvLEqA50Nxv2A',
   callbackUrl: 'http://localhost:3000/callback'
 };
